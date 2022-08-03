992b7ad301332aa5fe76540c8a07ca21dcb90bfb
David Gilmore <david.gilmore@outlook.com>
2022-08-03 11:20:28 +0100

    Adding pocket_actions.js - contains functions to interact with the getpocket.com API

---
+++ pocket_actions.js
@@ -0,0 +1,33 @@
+const https = require('https');
+
+function addItem(url) {
+  
+    var postData = JSON.stringify({
+      'url' : url
+    });
+  
+  const options = {
+    hostname: process.env.base_url,
+    path: `/${process.env.api_version}/add/?consumer_key=${process.env.consumer_key}&access_token=${process.env.access_token}`,
+    method: 'POST',
+    headers: {
+       'Content-Type': 'application/json',
+       'Content-Length': postData.length
+     }
+  };
+
+  var req = https.request(options, (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+  });
+  
+  req.on('error', (e) => {
+    console.error(e);
+  });
+  
+  req.write(postData);
+  req.end();
+  
+}
+
+module.exports = { addItem, };
